dcfade7d8813f407d81a0ef525153ee89ad14c43
zhaoheng <18722135306@163.com>
2020-04-27 23:56:20 +0800

    commit kafka util

---
+++ datatransfer-utils/src/main/java/com/datatransfer/utils/KafkaUtil.java
@@ -0,0 +1,18 @@
+package com.datatransfer.utils;
+
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KafkaUtil {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
